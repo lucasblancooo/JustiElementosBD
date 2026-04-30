9602e2a4730aec88cbd98574bc1d4ca525aa9b9f
lucasblancooo <lucas.blanco.et21.22@gmail.com>
2026-04-29 21:09:33 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,25 +1,13 @@
-
 <link rel="stylesheet" href="estilo.css">
 
-<div>
-<br>
-<h2 style="
-    margin: auto;
-    text-align: center;
-    color: rgb(175, 154, 77);
-    font-size: 300%;
-    "> Menú</h2>
-<br><br>
-</div>
+<div class="menu-wrapper">
 <table>
     <tr>
-        <td> <a href="index.php"> Home</a> </td>
-        <td> <a href="clientes.php"> Clientes</a> </td>
-        <td> <a href="prod.php"> Productos</a> </td>
-        <td> <a href="met_pago.php"> Método de pago</a> </td>
-         <td> <a href="orden.php"> Pedir</a> </td>
+        <td><a href="index.php">🏠 Home</a></td>
+        <td><a href="clientes.php">👤 Clientes</a></td>
+        <td><a href="prod.php">🔧 Productos</a></td>
+        <td><a href="met_pago.php">💳 Método de pago</a></td>
+        <td><a href="orden.php">📦 Pedir</a></td>
     </tr>
 </table>
-<br>
-<hr>
-<br><br>
+</div>
